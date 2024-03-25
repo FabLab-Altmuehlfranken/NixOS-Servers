a8ae1d2d5a6a3c0fb93cfaf7fdaf7fc84b231660
Ciapa <ciapa@lewd.wtf>
2024-03-25 17:16:50 +0100

    Add FabCI key to allowed root keys

--- defaults/users.nix
+++ defaults/users.nix
@@ -3,6 +3,7 @@
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHoKPipgr8EJNkLfG3qHTZxK9XTVDebVcic8mboiL1bg"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIERtfY26/h5xl+bzZm2htR4+Wd879DvZRPHsosFaEqIW"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA9N7KvKe7UZw6w1TBw44tJsr+Asrm7luHBoJj0rwrgE fabci"
     ];
   };
 }
